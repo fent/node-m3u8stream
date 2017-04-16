e6cf94c3d6e3042eaab4b27bb0110ed3bbd4ab13
fent <933490+fent@users.noreply.github.com>
2017-04-16 01:59:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# node-m3u8stream
+
+Concatenates segments from a [m3u8 playlist](https://tools.ietf.org/html/draft-pantos-http-live-streaming-20) into a consumable stream.
+
+[![Build Status](https://secure.travis-ci.org/fent/node-m3u8stream.svg)](http://travis-ci.org/fent/node-m3u8stream)
+[![Dependency Status](https://david-dm.org/fent/node-m3u8stream.svg)](https://david-dm.org/fent/node-m3u8stream)
+[![codecov](https://codecov.io/gh/fent/node-m3u8stream/branch/master/graph/badge.svg)](https://codecov.io/gh/fent/node-m3u8stream)
+
+
+# Usage
+
+```js
+const fs = require('fs');
+const m3u8stream = require('m3u8stream')
+
+m3u8stream('http://somesite.com/link/to/the/playlist.m3u8')
+    .pipe(fs.createWriteStream('videofile.mp4'));
+```
+
+
+# API
+
+### m3u8stream(url, [options])
+
+Creates a readable stream of binary media data. `options` can have the following
+
+* `chunkReadahead` - How many chunks to preload ahead. Default is `3`.
+* `highWaterMark` - How much of the download to buffer into the stream. See [node's docs](https://nodejs.org/api/stream.html#stream_constructor_new_stream_writable_options) for more. Note that the actual amount buffered can be higher since each chunk request maintains its own buffer.
+* `requestOptions` - Any options you want to pass to [miniget](https://github.com/fent/node-miniget), such as `headers` or `highWaterMark`.
+* `refreshInterval` - How often to refresh the playlist. If end of segment list is approached before the next interval, then it will refresh sooner.
+
+Stream has an `.end()` method, that if called, stops requesting segments, and refreshing the playlist.
+
+### Limitations
+
+Currently, it does not support [encrypted media segments](https://tools.ietf.org/html/draft-pantos-http-live-streaming-20#section-4.3.2.4). This is because the sites where this was tested on and intended for, YouTube and Twitch, don't use it.
+
+This does not parse master playlists, only media playlists. If you want to parse a master playlist to get links to media playlists, you can try the [m3u8 module](https://github.com/tedconf/node-m3u8), which m3u8stream uses.
+
+
+# Install
+
+    npm install m3u8stream
+
+
+# Tests
+Tests are written with [mocha](https://mochajs.org)
+
+```bash
+npm test
+```
+
+# License
+MIT
---
+++ test/m3u8-parser-test.js
@@ -0,0 +1,76 @@
+const m3u8parser = require('../lib/m3u8-parser');
+const fs         = require('fs');
+const path       = require('path');
+const assert     = require('assert');
+
+
+describe('m3u8-parser', function() {
+  describe('Parses tags from a simple playlist', function() {
+    it('Emits all tags and segments', function(done) {
+      var filepath = path.resolve(__dirname, 'playlists/simple.m3u8');
+      var tags = [];
+      var items = [];
+      var parser = new m3u8parser();
+      parser.on('tag', function(tag, value) {
+        tags.push({ tag: tag, value: value });
+      });
+      parser.on('item', function(item) {
+        items.push(item);
+      });
+      parser.on('error', done);
+      var rs = fs.createReadStream(filepath, { highWaterMark: 16 });
+      rs.pipe(parser);
+      rs.on('end', function() {
+        assert.deepEqual(tags, [
+          { tag: 'EXTM3U', value: null },
+          { tag: 'EXT-X-TARGETDURATION', value: '10' },
+          { tag: 'EXTINF', value: '9.009,' },
+          { tag: 'EXTINF', value: '9.009,' },
+          { tag: 'EXTINF', value: '3.003,' },
+          { tag: 'EXT-X-ENDLIST', value: null },
+        ]);
+        assert.deepEqual(items, [
+          'http://media.example.com/first.ts',
+          'http://media.example.com/second.ts',
+          'http://media.example.com/third.ts',
+        ]);
+        done();
+      });
+    });
+  });
+
+  describe('Parses tags from a live playlist', function() {
+    it('Emits all tags and segments', function(done) {
+      var filepath = path.resolve(__dirname, 'playlists/live-1.1.m3u8');
+      var tags = [];
+      var items = [];
+      var parser = new m3u8parser();
+      parser.on('tag', function(tag, value) {
+        tags.push({ tag: tag, value: value });
+      });
+      parser.on('item', function(item) {
+        items.push(item);
+      });
+      parser.on('error', done);
+      var rs = fs.createReadStream(filepath);
+      rs.pipe(parser);
+      rs.on('end', function() {
+        assert.deepEqual(tags, [
+          { tag: 'EXTM3U', value: null },
+          { tag: 'EXT-X-VERSION', value: '3' },
+          { tag: 'EXT-X-TARGETDURATION', value: '8' },
+          { tag: 'EXT-X-MEDIA-SEQUENCE', value: '2681' },
+          { tag: 'EXTINF', value: '7.975,' },
+          { tag: 'EXTINF', value: '7.941,' },
+          { tag: 'EXTINF', value: '7.975,' },
+        ]);
+        assert.deepEqual(items, [
+          'https://priv.example.com/fileSequence2681.ts',
+          'https://priv.example.com/fileSequence2682.ts',
+          'https://priv.example.com/fileSequence2683.ts',
+        ]);
+        done();
+      });
+    });
+  });
+});

c14be8f9cd59f11de1ec55f1d6d698f293b5b784
fent <933490+fent@users.noreply.github.com>
2017-05-01 23:25:41 -0400

    add discord.js example

---
+++ example/discord.js
@@ -0,0 +1,31 @@
+const Discord    = require('discord.js');
+const m3u8stream = require('..');
+ 
+var playlist = process.argv[2] || '';
+if (!playlist) {
+  const path = require('path');
+  var filepath = path.relative(process.cwd(), __filename);
+  console.error('Must provide link to  playlist');
+  console.error('node ' + filepath + ' [playlist]');
+
+} else {
+  const client = new Discord.Client();
+  client.login(' Y o u r   B o t   T o k e n ');
+
+  client.on('message', message => {
+    if (message.content.startsWith('++play')) {
+      const voiceChannel = message.member.voiceChannel;
+      if (!voiceChannel) {
+        return message.reply('Please be in a voice channel first!');
+      }
+      voiceChannel.join()
+        .then(connnection => {
+          let stream = m3u8stream(playlist);
+          const dispatcher = connnection.playStream(stream);
+          dispatcher.on('end', () => {
+            voiceChannel.leave();
+          });
+        });
+    }
+  });
+}

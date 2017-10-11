6d7646b2a6ae5b1f3a4875bfa3735b91fd9a3e81
Roly Fentanes <roly426@gmail.com>
2017-10-10 23:18:05 -0400

    update to node >=4

--- test/main-test.js
+++ test/main-test.js
@@ -8,17 +8,17 @@ const sinon      = require('sinon');
 function concat(stream, callback) {
   var body = '';
   stream.setEncoding('utf8');
-  stream.on('data', function(chunk) { body += chunk; });
+  stream.on('data', (chunk) => { body += chunk; });
   stream.on('error', callback);
-  stream.on('end', function() { callback(null, body); });
+  stream.on('end', () => { callback(null, body); });
 }
 
-describe('m3u8stream', function() {
-  before(function() { nock.disableNetConnect(); });
-  after(function() { nock.enableNetConnect(); });
+describe('m3u8stream', () => {
+  before(() => { nock.disableNetConnect(); });
+  after(() => { nock.enableNetConnect(); });
 
-  describe('Simple media playlist', function() {
-    it('Concatenates segments into stream', function(done) {
+  describe('Simple media playlist', () => {
+    it('Concatenates segments into stream', (done) => {
       var scope = nock('http://media.example.com')
         .get('/playlist.m3u8')
         .replyWithFile(200, path.resolve(__dirname, 'playlists/simple.m3u8'))
@@ -26,7 +26,7 @@ describe('m3u8stream', function() {
         .get('/second.ts').reply(200, 'two')
         .get('/third.ts').reply(200, 'three');
       var stream = m3u8stream('http://media.example.com/playlist.m3u8');
-      concat(stream, function(err, body) {
+      concat(stream, (err, body) => {
         assert.ifError(err);
         scope.done();
         assert.equal(body, 'onetwothree');
@@ -34,7 +34,7 @@ describe('m3u8stream', function() {
       });
     });
 
-    it('Concatenates relative segments into stream', function(done) {
+    it('Concatenates relative segments into stream', (done) => {
       var scope = nock('http://media.example.com')
         .get('/playlist.m3u8')
         .replyWithFile(200,
@@ -43,7 +43,7 @@ describe('m3u8stream', function() {
         .get('/second.ts').reply(200, 'two')
         .get('/third.ts').reply(200, 'three');
       var stream = m3u8stream('http://media.example.com/playlist.m3u8');
-      concat(stream, function(err, body) {
+      concat(stream, (err, body) => {
         assert.ifError(err);
         scope.done();
         assert.equal(body, 'onetwothree');
@@ -52,18 +52,18 @@ describe('m3u8stream', function() {
     });
   });
 
-  describe('Live media playlist', function() {
+  describe('Live media playlist', () => {
     var clock;
-    before(function() { clock = sinon.useFakeTimers(); });
-    after(function() { clock.restore(); });
+    before(() => { clock = sinon.useFakeTimers(); });
+    after(() => { clock.restore(); });
 
-    it('Refreshes after some time', function(done) {
+    it('Refreshes after some time', (done) => {
       var scope = nock('https://priv.example.com')
         .get('/playlist.m3u8')
         .replyWithFile(200, path.resolve(__dirname,
           'playlists/live-1.1.m3u8'))
         .get('/fileSequence2681.ts').reply(200, 'one')
-        .get('/fileSequence2682.ts').reply(function() {
+        .get('/fileSequence2682.ts').reply(() => {
           process.nextTick(passSomeTime);
           return 'two';
         });
@@ -82,7 +82,7 @@ describe('m3u8stream', function() {
         chunkReadahead: 1,
         refreshInterval: 1000 * 10,
       });
-      concat(stream, function(err, body) {
+      concat(stream, (err, body) => {
         assert.ifError(err);
         scope.done();
         assert.equal(body, 'onetwothreefourfive');
@@ -90,12 +90,12 @@ describe('m3u8stream', function() {
       });
     });
 
-    it('Refresh after nearing end of segment list', function(done) {
+    it('Refresh after nearing end of segment list', (done) => {
       var scope = nock('https://priv.example.com')
         .get('/playlist.m3u8')
         .replyWithFile(200, path.resolve(__dirname,
           'playlists/live-2.1.m3u8'))
-        .get('/fileSequence2681.ts').reply(function() {
+        .get('/fileSequence2681.ts').reply(() => {
           process.nextTick(passSomeTime);
           return 'apple';
         })
@@ -126,7 +126,7 @@ describe('m3u8stream', function() {
       }
 
       var stream = m3u8stream('https://priv.example.com/playlist.m3u8');
-      concat(stream, function(err, body) {
+      concat(stream, (err, body) => {
         assert.ifError(err);
         scope.done();
         assert.equal(body, [
@@ -155,28 +155,28 @@ describe('m3u8stream', function() {
       });
     });
 
-    it('Stops on error getting playlist', function(done) {
+    it('Stops on error getting playlist', (done) => {
       var scope = nock('http://mysite.com')
         .get('/pl.m3u8')
         .replyWithError('Nooo');
       var stream = m3u8stream('http://mysite.com/pl.m3u8');
-      stream.on('error', function(err) {
+      stream.on('error', (err) => {
         scope.done();
         assert.equal(err.message, 'Nooo');
         done();
       });
-      stream.on('end', function() {
+      stream.on('end', () => {
         throw Error('Should not emit end');
       });
     });
 
-    it('Stops on error refreshing playlist', function(done) {
+    it('Stops on error refreshing playlist', (done) => {
       var scope = nock('https://priv.example.com')
         .get('/playlist.m3u8')
         .replyWithFile(200, path.resolve(__dirname,
           'playlists/live-1.1.m3u8'))
-        .get('/fileSequence2681.ts').reply(function() {
-          process.nextTick(function() {
+        .get('/fileSequence2681.ts').reply(() => {
+          process.nextTick(() => {
             scope
               .get('/playlist.m3u8')
               .replyWithError('uh oh');
@@ -187,17 +187,17 @@ describe('m3u8stream', function() {
         .get('/fileSequence2683.ts').reply(200, 'three');
 
       var stream = m3u8stream('https://priv.example.com/playlist.m3u8');
-      stream.on('error', function(err) {
+      stream.on('error', (err) => {
         scope.done();
         assert.equal(err.message, 'uh oh');
         done();
       });
-      stream.on('end', function() {
+      stream.on('end', () => {
         throw new Error('Should not emit end');
       });
     });
 
-    it('Stops on error getting a segment', function(done) {
+    it('Stops on error getting a segment', (done) => {
       var scope = nock('https://priv.example.com')
         .get('/playme.m3u8')
         .replyWithFile(200, path.resolve(__dirname,
@@ -207,21 +207,21 @@ describe('m3u8stream', function() {
       var stream = m3u8stream('https://priv.example.com/playme.m3u8', {
         chunkReadahead: 1,
       });
-      stream.on('error', function(err) {
+      stream.on('error', (err) => {
         assert.equal(err.message, 'bad segment');
         scope.done();
         done();
       });
-      stream.on('end', function() {
+      stream.on('end', () => {
         throw new Error('Should not emit end');
       });
     });
 
-    describe('Destroy stream', function() {
-      describe('Right away', function() {
-        it('Ends stream right away with no data', function(done) {
+    describe('Destroy stream', () => {
+      describe('Right away', () => {
+        it('Ends stream right away with no data', (done) => {
           var stream = m3u8stream('https://whatever.com/playlist.m3u8');
-          concat(stream, function(err, body) {
+          concat(stream, (err, body) => {
             assert.ifError(err);
             assert.equal(body, '');
             done();
@@ -230,8 +230,8 @@ describe('m3u8stream', function() {
         });
       });
 
-      describe('In the middle of the segments list', function() {
-        it('Stops stream from emitting more data and ends it', function(done) {
+      describe('In the middle of the segments list', () => {
+        it('Stops stream from emitting more data and ends it', (done) => {
           var scope = nock('https://priv.example.com')
             .get('/playlist.m3u8')
             .replyWithFile(200, path.resolve(__dirname,
@@ -240,14 +240,14 @@ describe('m3u8stream', function() {
             .get('/fileSequence2682.ts').reply(200, 'banana')
             .get('/fileSequence2683.ts').reply(200, 'cherry')
             .get('/fileSequence2684.ts').reply(200, 'durango')
-            .get('/fileSequence2685.ts').reply(function() {
+            .get('/fileSequence2685.ts').reply(() => {
               stream.end();
               return 'whatever';
             });
           var stream = m3u8stream('https://priv.example.com/playlist.m3u8', {
             chunkReadahead: 1,
           });
-          concat(stream, function(err, body) {
+          concat(stream, (err, body) => {
             assert.ifError(err);
             scope.done();
             assert.equal(body, [
--- test/queue-test.js
+++ test/queue-test.js
@@ -3,23 +3,23 @@ const assert = require('assert');
 const sinon  = require('sinon');
 
 
-describe('Create a queue', function() {
-  describe('With 3 concurrency', function() {
+describe('Create a queue', () => {
+  describe('With 3 concurrency', () => {
     var clock;
-    before(function() { clock = sinon.useFakeTimers(); });
-    after(function() { clock.restore(); });
+    before(() => { clock = sinon.useFakeTimers(); });
+    after(() => { clock.restore(); });
 
-    it('Defaults to a set concurrency', function(done) {
+    it('Defaults to a set concurrency', (done) => {
       var maxms;
       var lastTask;
-      var q = new Queue(function(task, callback) {
+      var q = new Queue((task, callback) => {
         if (lastTask) {
           // Make sure tasks are called in order.
           // Even if they don't finish in order.
           assert.equal(lastTask, task - 1);
         }
         var ms = Math.floor(Math.random() * 1000);
-        setTimeout(function() { callback(null); }, ms);
+        setTimeout(() => { callback(null); }, ms);
         if (!maxms || ms > maxms) {
           maxms = ms;
         }
@@ -30,22 +30,22 @@ describe('Create a queue', function() {
         if (++called === total) {
           done();
         }
-        process.nextTick(function() { clock.tick(maxms); });
+        process.nextTick(() => { clock.tick(maxms); });
       }
 
       for (var i = 0; i < total; i++) {
         q.push(i, callback);
         assert.ok(q.active <= 3);
       }
-      process.nextTick(function() { clock.tick(maxms); });
+      process.nextTick(() => { clock.tick(maxms); });
     });
   });
 
-  describe('With 1 concurrency', function() {
-    it('Runs tasks sequentially one at a time', function(done) {
-      var q = new Queue(function(task, callback) {
+  describe('With 1 concurrency', () => {
+    it('Runs tasks sequentially one at a time', (done) => {
+      var q = new Queue((task, callback) => {
         assert.equal(q.active, 1);
-        process.nextTick(function() { callback(null); });
+        process.nextTick(() => { callback(null); });
       }, { concurrency: 1 });
 
       var total = 5, called = 0;
@@ -57,18 +57,18 @@ describe('Create a queue', function() {
     });
   });
 
-  describe('With `unique` option used', function() {
-    describe('Add same task while previous is running', function() {
-      it('Does not add the same tasks', function(done) {
+  describe('With `unique` option used', () => {
+    describe('Add same task while previous is running', () => {
+      it('Does not add the same tasks', (done) => {
         var total = 2, called = 0;
-        var q = new Queue(function(task, callback) {
-          process.nextTick(function() {
+        var q = new Queue((task, callback) => {
+          process.nextTick(() => {
             callback(null);
             if (++called === total) { done(); }
           });
         }, {
           concurrency: 10,
-          unique: function(task) { return task.id; },
+          unique: (task) => task.id,
         });
         q.push({ id: 4 });
         assert.equal(q.active, 1);
@@ -79,16 +79,16 @@ describe('Create a queue', function() {
       });
     });
 
-    describe('Add same task after previous finishes', function() {
-      it('Able to add same task again', function(done) {
-        var q = new Queue(function(task, callback) {
-          process.nextTick(function() { callback(null); });
+    describe('Add same task after previous finishes', () => {
+      it('Able to add same task again', (done) => {
+        var q = new Queue((task, callback) => {
+          process.nextTick(() => { callback(null); });
         }, {
           concurrency: 10,
-          unique: function(task) { return task.id; },
+          unique: (task) => task.id,
         });
-        q.push({ id: 4 }, function() {
-          process.nextTick(function() {
+        q.push({ id: 4 }, () => {
+          process.nextTick(() => {
             assert.equal(q.active, 0);
             q.push({ id: 4 }, done);
             assert.equal(q.active, 1);
@@ -99,11 +99,11 @@ describe('Create a queue', function() {
     });
   });
 
-  describe('Call worker callback twice', function() {
-    it('Calls task callback once', function(done) {
-      var q = new Queue(function(task, callback) {
+  describe('Call worker callback twice', () => {
+    it('Calls task callback once', (done) => {
+      var q = new Queue((task, callback) => {
         // Intentionally call callback twice.
-        process.nextTick(function() {
+        process.nextTick(() => {
           callback(null);
           callback(null);
         });
@@ -112,12 +112,12 @@ describe('Create a queue', function() {
     });
   });
 
-  describe('Kill it halfway', function() {
-    it('Does not run additional tasks', function(done) {
+  describe('Kill it halfway', () => {
+    it('Does not run additional tasks', (done) => {
       var results = [];
-      var q = new Queue(function(task, callback) {
+      var q = new Queue((task, callback) => {
         results.push(task);
-        process.nextTick(function() {
+        process.nextTick(() => {
           callback(null);
         });
       }, { concurrency: 2 });
@@ -130,7 +130,7 @@ describe('Create a queue', function() {
       assert.equal(q.tasks.length, 3);
       q.die();
       assert.equal(q.tasks.length, 0);
-      process.nextTick(function() {
+      process.nextTick(() => {
         assert.equal(q.active, 0);
         assert.deepEqual(results, ['a', 'b']);
         done();

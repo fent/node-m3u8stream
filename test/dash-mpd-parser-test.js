92f2a4a85880d1bd570bfc1c63d24d0f31f4c9e8
fent <933490+fent@users.noreply.github.com>
2018-09-26 01:54:51 -0400

    fix: include initialization segment in dash-mpd

--- test/dash-mpd-parser-test.js
+++ test/dash-mpd-parser-test.js
@@ -51,7 +51,7 @@ describe('dash MPD parser', () => {
         'playlists/multi-representation.mpd');
       let items = [];
       let endlist = false;
-      const parser = new DashMPDParser('133');
+      const parser = new DashMPDParser('140');
       parser.on('item', (item) => { items.push(item); });
       parser.on('endlist', () => { endlist = true; });
       parser.on('error', done);
@@ -60,29 +60,72 @@ describe('dash MPD parser', () => {
       rs.on('end', () => {
         assert.ok(endlist);
         assert.deepEqual(items, [
-          { url: 'https://videohost.com/133/0001.ts',
-            duration: 2000, seq: 1 },
-          { url: 'https://videohost.com/133/0002.ts',
+          { url: 'https://videohost.com/140/0000.ts',
+            duration: 0, seq: 1 },
+          { url: 'https://videohost.com/140/0001.ts',
             duration: 2000, seq: 2 },
-          { url: 'https://videohost.com/133/0003.ts',
+          { url: 'https://videohost.com/140/0002.ts',
             duration: 2000, seq: 3 },
-          { url: 'https://videohost.com/133/0004.ts',
+          { url: 'https://videohost.com/140/0003.ts',
             duration: 2000, seq: 4 },
-          { url: 'https://videohost.com/133/0005.ts',
+          { url: 'https://videohost.com/140/0004.ts',
             duration: 2000, seq: 5 },
-          { url: 'https://videohost.com/133/0006.ts',
+          { url: 'https://videohost.com/140/0005.ts',
             duration: 2000, seq: 6 },
-          { url: 'https://videohost.com/133/0007.ts',
+          { url: 'https://videohost.com/140/0006.ts',
             duration: 2000, seq: 7 },
-          { url: 'https://videohost.com/133/0008.ts',
+          { url: 'https://videohost.com/140/0007.ts',
             duration: 2000, seq: 8 },
-          { url: 'https://videohost.com/133/0009.ts',
+          { url: 'https://videohost.com/140/0008.ts',
             duration: 2000, seq: 9 },
-          { url: 'https://videohost.com/133/0010.ts',
+          { url: 'https://videohost.com/140/0009.ts',
             duration: 2000, seq: 10 },
+          { url: 'https://videohost.com/140/0010.ts',
+            duration: 2000, seq: 11 },
         ]);
         done();
       });
+    });
+
+    describe('With a representation with initialization segment', () => {
+      it('Emits all segments', (done) => {
+        let filepath = path.resolve(__dirname,
+          'playlists/multi-representation.mpd');
+        let items = [];
+        let endlist = false;
+        const parser = new DashMPDParser('133');
+        parser.on('item', (item) => { items.push(item); });
+        parser.on('endlist', () => { endlist = true; });
+        parser.on('error', done);
+        let rs = fs.createReadStream(filepath);
+        rs.pipe(parser);
+        rs.on('end', () => {
+          assert.ok(endlist);
+          assert.deepEqual(items, [
+            { url: 'https://videohost.com/133/0001.ts',
+              duration: 2000, seq: 1 },
+            { url: 'https://videohost.com/133/0002.ts',
+              duration: 2000, seq: 2 },
+            { url: 'https://videohost.com/133/0003.ts',
+              duration: 2000, seq: 3 },
+            { url: 'https://videohost.com/133/0004.ts',
+              duration: 2000, seq: 4 },
+            { url: 'https://videohost.com/133/0005.ts',
+              duration: 2000, seq: 5 },
+            { url: 'https://videohost.com/133/0006.ts',
+              duration: 2000, seq: 6 },
+            { url: 'https://videohost.com/133/0007.ts',
+              duration: 2000, seq: 7 },
+            { url: 'https://videohost.com/133/0008.ts',
+              duration: 2000, seq: 8 },
+            { url: 'https://videohost.com/133/0009.ts',
+              duration: 2000, seq: 9 },
+            { url: 'https://videohost.com/133/0010.ts',
+              duration: 2000, seq: 10 },
+          ]);
+          done();
+        })
+      })
     });
 
     describe('With a target representation that isn\'t found', () => {

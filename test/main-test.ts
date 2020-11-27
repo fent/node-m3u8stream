8d89e2f57f78b7947c3004a9181502f00c4e2095
TimeForANinja <TimeForANinja@users.noreply.github.com>
2020-11-27 16:26:24 +0100

    Update main-test.ts

--- test/main-test.ts
+++ test/main-test.ts
@@ -542,6 +542,6 @@ describe('m3u8stream', () => {
   });
 
   describe('Exposes parse-time', () => {
-    assert.equal(m3u8stream.parseHumanTime, humanStr);
+    assert.equal(m3u8stream.parseTimestamp, humanStr);
   });
 });
